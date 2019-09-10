be212e59df4321bcdc8e48be5aba885f883dc8ea
Mark Whitaker <mark@mainwave.co.uk>
2019-09-10 21:54:42 +0100

    Added custom test runner

---
+++ app/src/test/java/uk/co/mainwave/customjunitannotation/TicketTestRunner.kt
@@ -0,0 +1,22 @@
+package uk.co.mainwave.customjunitannotation
+
+import org.junit.runner.Description
+import org.junit.runners.BlockJUnit4ClassRunner
+import org.junit.runners.model.FrameworkMethod
+
+class TicketTestRunner(c: Class<*>) : BlockJUnit4ClassRunner(c) {
+    override fun describeChild(method: FrameworkMethod?): Description {
+        val testsTicketsAnnotation = method?.getAnnotation(TestsTickets::class.java)
+        return if (testsTicketsAnnotation == null) {
+            super.describeChild(method)
+        } else {
+            // Return the test name with the ticket names appended in brackets
+            val methodName = method.name
+            val ticketList = testsTicketsAnnotation.tickets.joinToString(", ")
+            Description.createTestDescription(
+                method.method.declaringClass,
+                "$methodName ($ticketList)"
+            )
+        }
+    }
+}
